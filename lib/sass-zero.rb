7dd3e960b3188b7a80a4e6e3e141adf2f2699999
Nixon <lazaronixon@hotmail.com>
2023-09-03 00:46:06 -0300

    add scaffold generators

--- lib/sass-zero.rb
+++ lib/sass-zero.rb
@@ -1,5 +1,9 @@
 require "sass_zero/version"
 
 module SassZero
-  class Engine < ::Rails::Engine; end
+  class Engine < ::Rails::Engine
+    config.app_generators do |g|
+      g.template_engine :sass_zero
+    end
+  end
 end
